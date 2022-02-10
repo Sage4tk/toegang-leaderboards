3c5f841113a7fb2706a1ff5ea670f3bed8bae8fc
timothy timbol <ttimbol08@gmail.com>
2022-02-10 10:59:06 +0400

    added join list function to join leaderboards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 //backend
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+import { useEffect, useState } from "react";
 
 //hooks
 import { useCollectionData } from "react-firebase-hooks/firestore"
@@ -8,35 +9,64 @@ import { useCollectionData } from "react-firebase-hooks/firestore"
 //db
 
 
-export default function Home({ auth, user, firestore }) {
-
-    
+export default function Home({ auth, user, firestore }) {    
     //firebase db
     const usersRef = firestore.collection("user");
     const query = usersRef.orderBy("numWins");
 
     const [board] = useCollectionData(query, {idField: 'id'});
 
+    //state for needed auth info
+    const [authInfo, setAuthInfo] = useState(null);
+
+    //when login is called set the user state
+    useEffect(() => {
+        if(user) {
+            setAuthInfo({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+                uid: user.uid
+            })
+        }
+    }, [user])
+
+    //const add user to db
+    const joinList = async() => {
+        await usersRef.add({
+            user: authInfo.displayName,
+            avatar: authInfo.photoURL,
+            uid: authInfo.uid,
+            numWins: 0
+        })
+    }
+
     return (
         <div>
             <h1>Home</h1>
-            <UserAuth auth={auth} />
-            <ul>
-                {board && board.map(data => (<PlayerCard />))}
-            </ul>
-            <AddPlayer user={user} />
+            <UserAuth auth={auth} setAuthInfo={setAuthInfo} />
+            <div>
+                {board && board.map(data => (<PlayerCard data={data} key={data.uid}/>))}
+            </div>
+            {user && <button onClick={joinList}>JOIN LIST</button>}
         </div>
     )
 }
 
-function UserAuth({ auth }) {
+function UserAuth({ auth, setAuthInfo }) {
     //sign in function
     const googleSignIn = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider);
     }
+
+    //signout function
+    const signOut = () => {
+        setAuthInfo(null);
+        auth.signOut()
+    }
+
     if (auth.currentUser) return (
-        <button onClick={() => auth.signOut()}>Sign-out</button>
+        <button onClick={signOut}>Sign-out</button>
     )
 
     return (
@@ -44,18 +74,11 @@ function UserAuth({ auth }) {
     )
 }
 
-function PlayerCard() {
+function PlayerCard({data}) {
     return (
-        <li>
-
-        </li>
-    )
-}
-
-//add player component
-function AddPlayer({user}) {
-    if (!user) return (null);
-    return(
-        <button>JOIN LEADERBOARDS</button>
+        <div>
+            <p>{data.user}</p>
+            <p>{data.numWins}</p>
+        </div>
     )
 }
