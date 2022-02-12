69db0a84119b87593d9ce6a86693c86cf0883c4b
timothy timbol <ttimbol08@gmail.com>
2022-02-12 18:10:51 +0400

    started admin login page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore"
 
 export default function Admin({ firestore }) {
@@ -5,7 +6,38 @@ export default function Admin({ firestore }) {
     const usersRef = firestore.collection("user");
     const query = usersRef.orderBy("numWins");
 
+    //admin userRef
+    const adminRef = firestore.collection('admin');
+
     const [board] = useCollectionData(query, {idField: 'id'});
+
+    //input checker
+    const [inputText, setInputText] = useState({
+        name: "",
+        password: ""
+    })
+
+    const inputChange = (e) => {
+        setInputText({
+            ...inputText,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    //auth check
+    const [logged, setLogged] = useState(false);
+
+
+
+    if (!logged) return (
+        <div>
+            <form onSubmit={}>
+                <input name="name" value={inputText.name} onChange={inputChange} />
+                <input name="password" value={inputText.password} onChange={inputChange} />
+                <button type="submit">LOG IN</button>
+            </form>
+        </div>
+    )
     return (
         <div>
             <h1>Admin page</h1>
